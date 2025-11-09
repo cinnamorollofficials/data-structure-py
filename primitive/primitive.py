3c6a9fd889487e11e5c76e388fc92e63c676a299
kuromiofficials <jtgunawan007@gmail.com>
2025-11-09 13:34:21 +0700

    first commit

---
+++ primitive/primitive.py
@@ -0,0 +1,18 @@
+# Integer
+age = 25
+print("Integer:", age, "| Type:", type(age))
+# Float
+height = 12.7
+print("Float:", height, "| Type:", type(height))
+
+# String
+name = "John"
+print("String:", name, "| Type:", type(name))
+
+# Boolean
+is_student = True
+print("Boolean:", is_student, "| Type:", type(is_student))
+
+# Complex Number
+z = 3 + 4j
+print("Complex Number:", z, "| Type:", type(z))
